b363af4260a55a41965b860a3f995b522f4d06b9
Pop0097 <dhruvrawat09@gmail.com>
2021-07-14 20:44:36 -0400

    first commit

---
+++ schedule_day/schedule_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedule_dayApp.swift
+//  schedule_day
+//
+//  Created by Dhruv Rawat on 2021-07-14.
+//
+
+import SwiftUI
+
+@main
+struct schedule_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
