65bd6b55a06a74f02fcbb2e734e6540b74f0e370
Pop0097 <dhruvrawat09@gmail.com>
2021-07-14 21:06:46 -0400

    Configured amplify

--- schedule_day/schedule_dayApp.swift
+++ schedule_day/schedule_dayApp.swift
@@ -5,13 +5,31 @@
 //  Created by Dhruv Rawat on 2021-07-14.
 //
 
+import Foundation
 import SwiftUI
+import Amplify
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        do {
+            Amplify.Logging.logLevel = .verbose
+            
+            try Amplify.configure()
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
+        return true
+    }
+}
 
 @main
-struct schedule_dayApp: App {
+struct MyAmplifyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
